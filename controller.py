228f8cbf9381df971d82bb7f6424bc36aa3aa6d9
Trent Robbins <robbintt@gmail.com>
2016-02-02 12:38:56 -0800

    Added dynamic route and moved data from route into template.

--- controller.py
+++ controller.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.debug = True 
 
 
-@app.route("/")
+@app.route("/hello")
+@app.route("/hello/<name>")
 def index(name=None):
     return render_template('hello.html', name=name)
 
