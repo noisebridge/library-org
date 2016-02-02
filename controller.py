b884dc4e4ad665093549939217f6f98a0f34d1f9
Trent Robbins <robbintt@gmail.com>
2016-02-02 13:01:03 -0800

    Added sqlalchemy sample app.

--- controller.py
+++ controller.py
@@ -1,12 +1,35 @@
 """
 """
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+sqlite_db = 'sqlite:////' + os.path.join(os.getcwd(), 'tmp', 'db.sqlite')
 
 app = Flask(__name__)
+
+#sqlalchemy configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_db
+db = SQLAlchemy(app)
+
 
 # flask will reload itself on changes when debug is True
 # flask can execute arbitrary code if you set this True
 app.debug = True 
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 @app.route("/hello")
---
+++ mkdb.py
@@ -0,0 +1,5 @@
+
+# From flask-sqlalchemy quickstart
+# http://flask-sqlalchemy.pocoo.org/2.1/quickstart/
+from controller import db
+db.create_all()
