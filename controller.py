cb84af409cf91d7274c55dedece38da0f0d828fe
Trent Robbins <robbintt@gmail.com>
2016-02-02 12:37:22 -0800

    Added the hello world template.

--- controller.py
+++ controller.py
@@ -1,15 +1,22 @@
-
-
-
-from flask import Flask
+"""
+"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# flask will reload itself on changes when debug is True
+# flask can execute arbitrary code if you set this True
+app.debug = True 
+
+
 @app.route("/")
-
-
-def hello():
-    return "Hello World!"
+def index(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
+    # flask can execute arbitrary python if you do this.
+    # app.run(host='0.0.0.0') # listens on all public IPs. 
+
     app.run()
+
+
