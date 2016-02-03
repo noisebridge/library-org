25041765596c40d4c3876cc0d2210d73ae17f42b
Trent Robbins <robbintt@gmail.com>
2016-02-03 14:13:09 -0800

    Added book class as a model using openlibrary fields.

--- controller.py
+++ controller.py
@@ -32,6 +32,35 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 
+class Book(db.Model):
+    """ Build a model based on the available fields in the openlibrary.org API.
+
+    Notes: 
+        authors - will be stored as a long string, openlibrary delivers it as a list of objects.
+
+    Additional info:
+        curl 'https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json'
+        using jscmd=data yields less info but is more stable
+        using jscmd=details will give us book cover thumbnail, preview url, table of contents, and more
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(20), unique=True)
+    title = db.Column(db.String(200), unique=True)
+    authors = db.Column(db.String(200), unique=True)
+    publish_date = db.Column(db.String(20), unique=True)
+    number_of_pages = db.Column(db.String(20), unique=True)
+
+    def __init__(self, isbn, title, authors, publish_date, number_of_pages):
+        self.isbn = isbn
+        self.title = title
+        self.authors = authors
+        self.publish_date = publish_date
+        self.number_of_pages = number_of_pages
+
+    def __repr__(self):
+        return '<Title: {}>'.format(self.title)
+
+
 @app.route("/hello")
 @app.route("/hello/<name>")
 def index(name=None):
